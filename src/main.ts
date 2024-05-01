f2b2dd054c558f219fd5f2a1925bb71d035a1111
Paulo Sergio <pauloserg861@gmail.com>
2024-05-01 15:06:43 -0300

    feat: add server configs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+const logger = new Logger('main.ts');
 
 async function bootstrap() {
+  const port = 3009;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3009);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
+  await app.listen(port).then(() => {
+    logger.log(`[API_SHOTLIFY] Server is running on http://localhost:${port}`);
+  });
 }
 bootstrap();
