11df9b61006132db85812846e0eb038bd247e52a
Paulo Sergio <pauloserg861@gmail.com>
2024-05-25 18:08:42 -0300

    chore: remove useless files

--- src/modules/user/infra/repositories/CreateUserRepository.ts
+++
@@ -1,5 +0,0 @@
-import { CreateUserDto } from '../../presentation/dtos/CreateUser.dto';
-
-export interface CreateUserRepository {
-  create(data: CreateUserDto): Promise<string>;
-}
--- src/modules/user/infra/repositories/VerifyUserRepository.ts
+++
@@ -1,5 +0,0 @@
-import { User } from '../../domain/entities/User';
-
-export interface VerifyUserRepository {
-  verify(data: Partial<User>): Promise<boolean>;
-}
