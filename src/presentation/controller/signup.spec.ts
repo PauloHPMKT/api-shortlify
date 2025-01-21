c84db87061e99284fc2968df35656fa28436eeca
Paulo Sergio <pauloserg861@gmail.com>
2025-01-20 21:28:12 -0300

    refactor: move sut to a factory method

--- src/presentation/controller/signup.spec.ts
+++ src/presentation/controller/signup.spec.ts
@@ -1,8 +1,17 @@
 import { SignupController } from './signup';
 
+const makeSut = (): SutTypes => {
+  const sut = new SignupController();
+  return { sut };
+};
+
+interface SutTypes {
+  sut: SignupController;
+}
+
 describe('Signup Controller', () => {
   it('should be defined', () => {
-    const sut = new SignupController();
+    const sut = makeSut();
     expect(sut).toBeDefined();
   });
 });
