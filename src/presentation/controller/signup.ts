1ff61796bb2b98e0ff1d529517e8f29cf610252c
Paulo Sergio <pauloserg861@gmail.com>
2025-01-20 21:43:26 -0300

    feat: ensure SignupController return an error message if no name is provided

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -5,6 +5,7 @@ export class SignupController {
       if (!httpRequest.body[field]) {
         return {
           statusCode: 400,
+          body: new Error(`Missing param: ${field}`),
         };
       }
     }
