0c905ce6a6cc166769b9f1c4c5174f142328d8bd
Paulo Sergio <pauloserg861@gmail.com>
2025-01-23 22:30:42 -0300

    test: ensure DbAddAccount call Encrypter with correct password

--- src/data/usecases/add-aacount/db-add-account.spec.ts
+++ src/data/usecases/add-aacount/db-add-account.spec.ts
@@ -1,19 +1,44 @@
+import { Encrypter } from '../../protocols/encrypter';
 import { DbAddAccount } from './db-add-account';
 
+const makeEncrypter = (): Encrypter => {
+  class EncrypterStub implements Encrypter {
+    async encrypt(value: string): Promise<string> {
+      return new Promise((resolve) => resolve('hashed_password'));
+    }
+  }
+  return new EncrypterStub();
+};
+
 const makeSut = (): SutTypes => {
-  const sut = new DbAddAccount();
+  const encrypterStub = makeEncrypter();
+  const sut = new DbAddAccount(encrypterStub);
   return {
     sut,
+    encrypterStub,
   };
 };
 
 interface SutTypes {
   sut: DbAddAccount;
+  encrypterStub: Encrypter;
 }
 
 describe('DbAddAccount Usecase', () => {
   it('should be defined', () => {
     const { sut } = makeSut();
     expect(sut).toBeDefined();
+  });
+
+  it('should call Encrypter with correct password', async () => {
+    const { sut, encrypterStub } = makeSut();
+    const encryptSpy = jest.spyOn(encrypterStub, 'encrypt');
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'hashed_password',
+    };
+    await sut.add(accountData);
+    expect(encryptSpy).toHaveBeenCalledWith('hashed_password');
   });
 });
