4a4bab169bb010aeef1774225312162c3164968c
Paulo Sergio <pauloserg861@gmail.com>
2025-01-25 11:25:45 -0300

    test: ensure DdAddAccount throw Encrypter throws

--- src/data/usecases/add-aacount/db-add-account.spec.ts
+++ src/data/usecases/add-aacount/db-add-account.spec.ts
@@ -41,4 +41,20 @@ describe('DbAddAccount Usecase', () => {
     await sut.add(accountData);
     expect(encryptSpy).toHaveBeenCalledWith('hashed_password');
   });
+
+  it('should throw if Encrypter throws', async () => {
+    const { sut, encrypterStub } = makeSut();
+    jest
+      .spyOn(encrypterStub, 'encrypt')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error())),
+      );
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'hashed_password',
+    };
+    const promise = sut.add(accountData);
+    await expect(promise).rejects.toThrow();
+  });
 });
