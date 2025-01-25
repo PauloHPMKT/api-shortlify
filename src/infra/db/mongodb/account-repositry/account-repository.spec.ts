ff49c0aad370ed401f8586b558b5aea6732a5657
Paulo Sergio <pauloserg861@gmail.com>
2025-01-25 16:47:48 -0300

    test: ensure AccountRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repositry/account-repository.spec.ts
@@ -0,0 +1,45 @@
+import { AccountModel } from '../../../../domain/models/account';
+import { MongoHelper } from '../helpers/mongo-helper';
+import { AccountMongoRepository } from './account-repository';
+
+const makeSut = (): AccountMongoRepository => {
+  return new AccountMongoRepository();
+};
+
+describe('AccountRepository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  it('should be defined', () => {
+    const sut = makeSut();
+    expect(sut).toBeDefined();
+  });
+
+  it('should return an account on success', async () => {
+    const sut = makeSut();
+    const createdAt = new Date();
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email',
+      password: 'any_password',
+      shortenLinks: [],
+      avatar: null,
+      isActive: true,
+      createdAt,
+    });
+    expect(account).toBeTruthy();
+    expect(account.id).toBeTruthy();
+    expect(account.name).toBe('any_name');
+    expect(account.email).toBe('any_email');
+    expect(account.password).toBe('any_password');
+    expect(account.shortenLinks).toEqual([]);
+    expect(account.avatar).toBeNull();
+    expect(account.isActive).toBe(true);
+    expect(account.createdAt).toBeInstanceOf(Date);
+  });
+});
