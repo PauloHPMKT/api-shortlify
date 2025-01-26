6f07dfba0f5a95dda67bcc63fdd70d6675ea0f93
Paulo Sergio <pauloserg861@gmail.com>
2025-01-26 19:31:17 -0300

    test: ensure CreateShortenLinkController return 400 if no originalUrl is provided

--- src/presentation/controller/shorten/create-shorten-link.spec.ts
+++ src/presentation/controller/shorten/create-shorten-link.spec.ts
@@ -1,3 +1,4 @@
+import { MissingParamError } from '../../errors';
 import { CreateShortenLinkController } from './create-shorten-link';
 
 const makeSut = (): CreateShortenLinkController => {
@@ -8,5 +9,18 @@ describe('CreateShortenLinkController', () => {
   it('should be defined', () => {
     const sut = makeSut();
     expect(sut).toBeDefined();
+  });
+
+  it('should return 400 if no url is provided', async () => {
+    const sut = makeSut();
+    const httpRequest = {
+      body: {
+        originalUrl: '',
+        accountId: 'any_account_id',
+      },
+    };
+    const response = await sut.handle(httpRequest);
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toEqual(new MissingParamError('originalUrl'));
   });
 });
