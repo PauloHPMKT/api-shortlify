2331d3b4b7798e2a3829f284e7499c25b8ea99e4
Paulo Sergio <pauloserg861@gmail.com>
2025-01-26 19:31:01 -0300

    feat: ensure CreateShortenLinkController return 400 if no originalUrl is provided

--- src/presentation/controller/shorten/create-shorten-link.ts
+++ src/presentation/controller/shorten/create-shorten-link.ts
@@ -1,1 +1,11 @@
-export class CreateShortenLinkController {}
+import { MissingParamError } from '../../errors';
+import { badRequest } from '../../helpers/http-responses';
+import { Controller, HttpRequest, HttpResponse } from '../../protocols';
+
+export class CreateShortenLinkController implements Controller {
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    if (!httpRequest.body.originalUrl) {
+      return badRequest(new MissingParamError('originalUrl'));
+    }
+  }
+}
