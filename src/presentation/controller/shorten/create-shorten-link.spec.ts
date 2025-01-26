eaac68eddb7188743dd95bffd310762b2a027906
Paulo Sergio <pauloserg861@gmail.com>
2025-01-26 19:44:32 -0300

    test: ensure CreateShortenLinkController return 400 if no accountId is provided

--- src/presentation/controller/shorten/create-shorten-link.spec.ts
+++ src/presentation/controller/shorten/create-shorten-link.spec.ts
@@ -15,7 +15,6 @@ describe('CreateShortenLinkController', () => {
     const sut = makeSut();
     const httpRequest = {
       body: {
-        originalUrl: '',
         accountId: 'any_account_id',
       },
     };
@@ -29,7 +28,45 @@ describe('CreateShortenLinkController', () => {
     const httpRequest = {
       body: {
         originalUrl: 'any_url',
+      },
+    };
+    const response = await sut.handle(httpRequest);
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toEqual(new MissingParamError('accountId'));
+  });
+
+  it('should return 400 if accountId is empty', async () => {
+    const sut = makeSut();
+    const httpRequest = {
+      body: {
+        originalUrl: 'any_url',
         accountId: '',
+      },
+    };
+    const response = await sut.handle(httpRequest);
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toEqual(new MissingParamError('accountId'));
+  });
+
+  it('should return 400 if accountId is null', async () => {
+    const sut = makeSut();
+    const httpRequest = {
+      body: {
+        originalUrl: 'any_url',
+        accountId: null,
+      },
+    };
+    const response = await sut.handle(httpRequest);
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toEqual(new MissingParamError('accountId'));
+  });
+
+  it('should return 400 if accountId is undefined', async () => {
+    const sut = makeSut();
+    const httpRequest = {
+      body: {
+        originalUrl: 'any_url',
+        accountId: undefined,
       },
     };
     const response = await sut.handle(httpRequest);
