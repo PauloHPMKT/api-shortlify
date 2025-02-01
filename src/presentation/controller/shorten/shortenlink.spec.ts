127b5a6b6106fe1b8eb327dffab2c557b962c99b
Paulo Sergio <pauloserg861@gmail.com>
2025-02-01 13:58:57 -0300

    test: ensure ShortenLinkController is defined

---
+++ src/presentation/controller/shorten/shortenlink.spec.ts
@@ -0,0 +1,8 @@
+import { ShortenLinkController } from './shortenlink';
+
+describe('ShortenLinkController', () => {
+  it('should be defined', () => {
+    const sut = new ShortenLinkController();
+    expect(sut).toBeDefined();
+  });
+});
