a5944e532cc254fd7d3a16f03e6ffe37402ed5d4
Paulo Sergio <pauloserg861@gmail.com>
2025-02-01 16:25:13 -0300

    feat: ensure GenerateBitlinkController returns 400 if long_url is invalid

--- src/presentation/controller/shorten/generate.ts
+++ src/presentation/controller/shorten/generate.ts
@@ -1,11 +1,18 @@
-import { MissingParamError } from '../../errors';
+import { InvalidParamError, MissingParamError } from '../../errors';
 import { badRequest } from '../../helpers/http-responses';
 import { HttpRequest, HttpResponse } from '../../protocols';
+import { UrlValidator } from '../../protocols/url-validator';
 
 export class GenerateBitlinkController {
+  constructor(private readonly urlValidator: UrlValidator) {}
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     if (!httpRequest.body.long_url) {
       return badRequest(new MissingParamError('long_url'));
+    }
+    const isUrlValid = this.urlValidator.isValid(httpRequest.body.long_url);
+    if (!isUrlValid) {
+      return badRequest(new InvalidParamError('long_url'));
     }
   }
 }
