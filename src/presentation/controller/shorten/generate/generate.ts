0980b0fc215bcb7eed36cbd47ccdc1186b134a96
Paulo Sergio <pauloserg861@gmail.com>
2025-02-01 16:02:25 -0300

    feat: ensure GenerateBitlinkController returns 400 if no long_url is provided

--- src/presentation/controller/shorten/generate/generate.ts
+++ src/presentation/controller/shorten/generate/generate.ts
@@ -1,1 +1,13 @@
-export class GenerateBitlinkController {}
+import { MissingParamError } from '../../../errors';
+import { HttpRequest, HttpResponse } from '../../../protocols';
+
+export class GenerateBitlinkController {
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    if (!httpRequest.body.long_url) {
+      return {
+        statusCode: 400,
+        body: new MissingParamError('long_url'),
+      };
+    }
+  }
+}
