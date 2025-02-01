c9e5cc64844ae50df99babcaf730bc1b3e6f84b8
Paulo Sergio <pauloserg861@gmail.com>
2025-02-01 16:06:41 -0300

    refactor: move http response to a helper method

--- src/presentation/controller/shorten/generate.ts
+++ src/presentation/controller/shorten/generate.ts
@@ -1,13 +1,11 @@
 import { MissingParamError } from '../../errors';
+import { badRequest } from '../../helpers/http-responses';
 import { HttpRequest, HttpResponse } from '../../protocols';
 
 export class GenerateBitlinkController {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     if (!httpRequest.body.long_url) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('long_url'),
-      };
+      return badRequest(new MissingParamError('long_url'));
     }
   }
 }
