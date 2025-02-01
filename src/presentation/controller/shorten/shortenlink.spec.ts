6b7a5c39944a691d7f1a67c1af14449cdd4b450e
Paulo Sergio <pauloserg861@gmail.com>
2025-02-01 14:10:37 -0300

    test: ensure ShortenLinkController returns 400 if no url is provided

--- src/presentation/controller/shorten/shortenlink.spec.ts
+++ src/presentation/controller/shorten/shortenlink.spec.ts
@@ -5,4 +5,17 @@ describe('ShortenLinkController', () => {
     const sut = new ShortenLinkController();
     expect(sut).toBeDefined();
   });
+
+  it('should return 400 if no url is provided', async () => {
+    const sut = new ShortenLinkController();
+    const httpRequest = {
+      body: {
+        url: '',
+        accountId: 'any_account_id',
+      },
+    };
+    const response = await sut.handle(httpRequest);
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toEqual(new Error('Missing param: url'));
+  });
 });
