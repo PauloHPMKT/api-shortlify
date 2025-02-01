fb5baa2a44f78b641776a6bdb5021b76e4ff86df
Paulo Sergio <pauloserg861@gmail.com>
2025-02-01 14:10:19 -0300

    feat: ensure ShortenLinkController returns 400 if no url is provided

--- src/presentation/controller/shorten/shortenlink.ts
+++ src/presentation/controller/shorten/shortenlink.ts
@@ -1,1 +1,12 @@
-export class ShortenLinkController {}
+import { HttpRequest, HttpResponse } from '../../protocols';
+
+export class ShortenLinkController {
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    if (!httpRequest.body.url) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: url'),
+      };
+    }
+  }
+}
