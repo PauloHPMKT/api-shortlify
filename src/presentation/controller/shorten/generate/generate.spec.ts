eae88a0bf2f16dd4b2d62c0e57a15988c83e6d5e
Paulo Sergio <pauloserg861@gmail.com>
2025-02-01 16:02:54 -0300

    test: ensure GenerateBitlinkController returns 400 if no long_url is provided

--- src/presentation/controller/shorten/generate/generate.spec.ts
+++ src/presentation/controller/shorten/generate/generate.spec.ts
@@ -1,3 +1,4 @@
+import { MissingParamError } from '../../../errors';
 import { GenerateBitlinkController } from './generate';
 
 const makeSut = (): GenerateBitlinkController => {
@@ -8,5 +9,17 @@ describe('GenerateBitlinkController', () => {
   it('should be defined', () => {
     const sut = makeSut();
     expect(sut).toBeDefined();
+  });
+
+  it('should return 400 if no long_url is provided', async () => {
+    const sut = makeSut();
+    const httpRequest = {
+      body: {
+        long_url: '',
+      },
+    };
+    const response = await sut.handle(httpRequest);
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toEqual(new MissingParamError('long_url'));
   });
 });
