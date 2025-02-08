d23a088a96a526304f9cd9407965c929a383f2bd
Paulo Sergio <pauloserg861@gmail.com>
2025-02-08 16:43:13 -0300

    feat: ensure BitlyServise calls fetch with correct options

--- src/infra/bitly/bitly-service.ts
+++ src/infra/bitly/bitly-service.ts
@@ -1,1 +1,18 @@
-export class BitlyService {}
+import { ShortenLink } from '../../data/protocols/shortenlink';
+
+export class BitlyService implements ShortenLink {
+  private readonly apiUrl = process.env.BITLY_URL;
+  private readonly apiKey = process.env.BITLY_TOKEN;
+
+  async shorten(url: string): Promise<any> {
+    const response = await fetch(`${this.apiUrl}/shorten`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${this.apiKey}`,
+      },
+      body: JSON.stringify({ long_url: url }),
+    });
+    return response;
+  }
+}
