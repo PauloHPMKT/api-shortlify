fa82675cabefa446b8a13a683ce3823d45953862
Paulo Sergio <pauloserg861@gmail.com>
2025-02-08 21:01:10 -0300

    fix: adjust fetch bitly service return

--- src/main/routes/shorten-routes.test.ts
+++ src/main/routes/shorten-routes.test.ts
@@ -2,6 +2,15 @@ import request from 'supertest';
 import app from '../config/app';
 
 describe('Shorten Routes', () => {
+  beforeEach(() => {
+    global.fetch = jest.fn().mockResolvedValue({
+      json: jest.fn().mockResolvedValue({
+        id: 'bit.ly/xyz123',
+        link: 'https://bit.ly/xyz123',
+      }),
+    }) as any;
+  });
+
   it('should return 200 on success', async () => {
     await request(app)
       .post('/api/shorten')
