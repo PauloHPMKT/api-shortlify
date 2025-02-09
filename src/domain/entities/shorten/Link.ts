fd2fd698e79e717b2f0b0c1805295a644f6b44e1
Paulo Sergio <pauloserg861@gmail.com>
2025-02-09 17:19:11 -0300

    refactor: modify data structure in entity

--- src/domain/entities/shorten/Link.ts
+++ src/domain/entities/shorten/Link.ts
@@ -1,20 +1,27 @@
 export class Link {
-  public readonly id: string;
   public link: string;
   public long_url: string;
   public custom_id: string;
   public references: ShortenLink.GroupTypes;
   public archived?: boolean;
-  public tags?: any[];
+  public tags?: string[];
   public created_at?: Date;
 
-  constructor(props: any) {
+  constructor(props: Link) {
     Object.assign(this, props);
+    this.created_at = props.created_at || new Date();
+    this.archived = props.archived ?? false;
+    this.tags = props.tags ?? [];
   }
 }
 
 export namespace ShortenLink {
   export interface GroupTypes {
     group: string;
+  }
+
+  export interface Tag {
+    name: string;
+    id: string;
   }
 }
